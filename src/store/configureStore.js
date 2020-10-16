89fde0ab2edddae22e8dd10a6158dbb5e7a03fe6
AssanSav <assane.savadogo81@gmail.com>
2020-10-16 19:47:06 -0400

    Redux store set to use custom made middlewares

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import reducer from './reducer';
-
+import logger from './middleware/logger';
+import func from "./middleware/func";
+import toast from "./middleware/toast";
+import api from "./middleware/api"
 
 const store = () => {
-    return configureStore({ reducer })
+  return configureStore({
+    reducer,
+    middleware: [
+      ...getDefaultMiddleware(),
+      // logger,
+      // func,
+      // toast,
+      api
+    ]
+  })
 }
 
 export default store()
